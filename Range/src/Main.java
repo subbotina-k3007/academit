591650aa2789974d3d104cf3a21919bcc27c1260
subbotina_k3007@mail.ru <qcBZof02>
2021-04-19 23:01:46 +0300

    Добавлена вторая часть задачи Range

--- Range/src/Main.java
+++ Range/src/Main.java
@@ -1,23 +1,40 @@
 import ru.academits.Subbotina.range.Range;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
         Range range = new Range(3.2, 8.9);
 
-        System.out.println ("Первоначальное начало диапазона = " + range.getFrom());
-        System.out.println ("Первоначальный конец диапазона = " + range.getTo());
-        System.out.println ("Первоначальая длина диапазона = " + range.getLength());
+        System.out.println("Первоначальное начало диапазона = " + range.getFrom());
+        System.out.println("Первоначальный конец диапазона = " + range.getTo());
+        System.out.println("Первоначальная длина диапазона = " + range.getLength());
 
         range.setFrom(1.2);
         range.setTo(15.7);
 
-        System.out.println ("Новое начало диапазона = " + range.getFrom());
-        System.out.println ("Новый конец диапазона = " + range.getTo());
-        System.out.println ("Новая длина диапазона = " + range.getLength());
+        System.out.println("Новое начало диапазона = " + range.getFrom());
+        System.out.println("Новый конец диапазона = " + range.getTo());
+        System.out.println("Новая длина диапазона = " + range.getLength());
 
-        System.out.println (range.isInside(5.3));
-        System.out.println (range.isInside(15.7));
-        System.out.println (range.isInside(15.71));
+        System.out.println(range.isInside(5.3));
+        System.out.println(range.isInside(15.7));
+        System.out.println(range.isInside(15.71));
 
+        Range range2 = new Range(7, 19);
+
+        Range range3 = range.getIntersection(range2);
+
+        if (range3 == null) {
+            System.out.println("null");
+        } else {
+            System.out.println(range3.toString());
+        }
+
+        Range range4 = new Range(-15.8, 1.2);
+        System.out.println(Arrays.toString(range.getUnion(range4)));
+
+        Range range5 = new Range(3, 5);
+        System.out.println(Arrays.toString(range.getDifference(range5)));
     }
 }
--- Range/src/ru/academits/Subbotina/range/Range.java
+++ Range/src/ru/academits/Subbotina/range/Range.java
@@ -4,16 +4,16 @@ public class Range {
     double from;
     double to;
 
-    public Range (double from, double to) {
+    public Range(double from, double to) {
         this.from = from;
         this.to = to;
     }
 
-    public void setFrom (double from) {
+    public void setFrom(double from) {
         this.from = from;
     }
 
-    public void setTo (double to) {
+    public void setTo(double to) {
         this.to = to;
     }
 
@@ -29,7 +29,42 @@ public class Range {
         return (to - from);
     }
 
+    @Override
+    public String toString() {
+        return "from " + from + " to " + to;
+    }
+
     public boolean isInside(double number) {
         return (number >= from && number <= to);
+    }
+
+    public Range getIntersection(Range range2) {
+        if (this.to <= range2.getFrom() || range2.getTo() <= this.from) {
+            return null;
+        }
+
+        return new Range(Math.max(this.from, range2.getFrom()), Math.min(this.to, range2.getTo()));
+    }
+
+    public Range[] getUnion(Range range2) {
+        if (this.to < range2.getFrom() || range2.getTo() < this.from) {
+            return new Range[]{new Range(from, to), range2};
+        } else {
+            return new Range[]{new Range(Math.min(this.from, range2.getFrom()), Math.max(this.to, range2.getTo()))};
+        }
+    }
+
+    public Range[] getDifference(Range range2) {
+        if (this.to <= range2.getFrom() || range2.getTo() <= this.from) {
+            return new Range[]{new Range(from, to)};
+        } else if (this.from < range2.getFrom() && this.to > range2.getTo()) {
+            return new Range[]{new Range(from, range2.getFrom()), new Range(range2.getTo(), to)};
+        } else if (this.from < range2.getFrom() && this.to < range2.getTo()) {
+            return new Range[]{new Range(from, range2.getFrom())};
+        } else if (this.from > range2.getFrom() && this.to > range2.getTo()) {
+            return new Range[]{new Range(range2.getTo(), to)};
+        } else {
+            return null;
+        }
     }
 }
